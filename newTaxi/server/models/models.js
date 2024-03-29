2420b06b95559046be87fa9543a06a61b9c6bf92
SSSkeletry <skeletryz@gmail.com>
2024-03-29 18:58:41 +0200

    change in database

--- newTaxi/server/models/models.js
+++ newTaxi/server/models/models.js
@@ -2,29 +2,26 @@ const sequelize = require('../database')
 const {DataTypes} = require('sequelize')
 
 const User = sequelize.define('user',{
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    email: {type: DataTypes.STRING,unique: true},
+    email: {type: DataTypes.STRING,primaryKey: true,unique: true,allowNull:false},
     password: {type: DataTypes.STRING},
     phone: {type: DataTypes.CHAR,unique: true},
     role: {type: DataTypes.STRING,defaultValue: 'USER'}
 })
 
 const Car = sequelize.define('car',{
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    number_car: {type: DataTypes.STRING,allowNull:false},
+    number_car: {type: DataTypes.STRING,primaryKey: true,allowNull:false},
     name_car: {type: DataTypes.CHAR,allowNull:false},
     class_car: {type: DataTypes.CHAR,allowNull:false},
     img: {type: DataTypes.STRING,allowNull:false}
 })
 const CarInfo = sequelize.define('car_info',{
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    car_number: {type: DataTypes.STRING, primaryKey: true, allowNull:false},
     title: {type: DataTypes.STRING, allowNull:false},
     description: {type: DataTypes.STRING, allowNull:false}
     
 })
 const Driver = sequelize.define('driver',{
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    email: {type: DataTypes.STRING,unique: true},
+    email: {type: DataTypes.STRING,primaryKey: true,unique: true,allowNull:false},
     password: {type: DataTypes.STRING},
     phone: {type: DataTypes.CHAR,unique: true},
     rating: {type: DataTypes.CHAR},
@@ -32,24 +29,20 @@ const Driver = sequelize.define('driver',{
 })
 
 const Dispatcher = sequelize.define('dispatcher',{
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    email: {type: DataTypes.STRING,unique: true},
+    email: {type: DataTypes.STRING,primaryKey: true,unique: true,allowNull:false},
     password: {type: DataTypes.STRING},
     phone: {type: DataTypes.CHAR,unique: true},
     warning: {type: DataTypes.CHAR},
     role: {type: DataTypes.STRING,defaultValue: 'DISPATCHER'}
 })
-const Data = sequelize.define('data',{
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    start_place: {type: DataTypes.STRING, allowNull:false},
-    end_place: {type: DataTypes.STRING, allowNull:false},
-    price: {type: DataTypes.CHAR},
-})
 const Order = sequelize.define('order',{
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    idOrder: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true,allowNull:false},
     start_time: {type: DataTypes.DATE},
     end_time: {type: DataTypes.DATE},
     trip_rate: {type: DataTypes.CHAR},
+    start_place: {type: DataTypes.STRING, allowNull:false},
+    end_place: {type: DataTypes.STRING, allowNull:false},
+    price: {type: DataTypes.CHAR},
     complete: {type: DataTypes.STRING}
 })
 
@@ -57,25 +50,21 @@ const Order = sequelize.define('order',{
 Car.hasMany(Driver);
 Driver.belongsTo(Car)
 
-User.hasMany(Data);
-Driver.hasMany(Data);
-Dispatcher.hasMany(Data);
-Data.belongsTo(User);
-Data.belongsTo(Driver);
-Data.belongsTo(Dispatcher);
+User.hasMany(Order);
+Driver.hasMany(Order);
+Dispatcher.hasMany(Order);
+Order.belongsTo(User);
+Order.belongsTo(Driver);
+Order.belongsTo(Dispatcher);
 
-Car.hasMany(CarInfo, {as: 'info'});
-CarInfo.belongsTo(Car);
-
-Data.hasMany(Order);
-Order.belongsTo(Data)
+Car.hasMany(CarInfo, {foreignKey: 'car_number', as: 'info'});
+CarInfo.belongsTo(Car,{ foreignKey: 'car_number', targetKey: 'number_car' });
 
 module.exports = {
     User,
     Dispatcher,
     Car,
     Driver,
-    Data,
     Order
 }
 
