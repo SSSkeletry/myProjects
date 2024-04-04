2e3f8039bac1100eb54e2a9240c3adb45607ca9d
SSSkeletry <skeletryz@gmail.com>
2024-04-04 14:17:31 +0300

    delete data

--- newTaxi/server/routes/dataRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const dataControl = require('../controllers/dataControl');
-
-router.post('/',dataControl.create)
-router.get('/',dataControl.getOne)
-
-module.exports = router;
