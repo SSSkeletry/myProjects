0c51075317a1061ff0b9212cc4fc9cf861ff57a8
SSSkeletry <skeletryz@gmail.com>
2024-05-23 21:38:41 +0300

    change folder

---
+++ newTaxi/client/src/route.js
@@ -0,0 +1,69 @@
+import User from "./pages/user"
+import Login from "./pages/login"
+import Profile from "./pages/profile"
+import Order from "./pages/order"
+import Taxi from "./pages/taxi"
+import Dispatcher from "./pages/dispatcher"
+import Driver from "./pages/driver"
+import { LOGIN_ROUTE, ORDER_ROUTE, PROFILE_ROUTE, REGISTRATION_ROUTE, TAXI_ROUTE, USER_ROUTE,DISPATCHER_ROUTE, DRIVER_ROUTE } from "./utils/consts";
+
+
+export const authRoutes = [
+    {
+        path: USER_ROUTE,
+        Component: User,
+
+    },
+    {
+        path: PROFILE_ROUTE,
+        Component: Profile,
+    },
+    {
+        path: ORDER_ROUTE,
+        Component: Order,
+
+    },
+]
+
+export const pubRoutes = [
+    {
+        path: LOGIN_ROUTE,
+        Component: Login,
+    },
+    {
+        path: REGISTRATION_ROUTE,
+        Component: Login,
+
+    },
+    {
+        path: TAXI_ROUTE,
+        Component: Taxi,
+    },
+    
+]
+
+export const DispatcherRoutes = [
+    {
+        path: PROFILE_ROUTE,
+        Component: Profile,
+    },
+    {
+        path: ORDER_ROUTE,
+        Component: Order,
+    },
+    {
+        path: DISPATCHER_ROUTE,
+        Component: Dispatcher,
+    },
+]
+
+export const driverRoutes = [
+    {
+        path: PROFILE_ROUTE,
+        Component: Profile,
+    },
+    {
+        path: DRIVER_ROUTE,
+        Component: Driver,
+    },
+]
